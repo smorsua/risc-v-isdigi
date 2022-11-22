94137974ba3e648ffa669bbf36a4e11c7f699c8a
luisgaarrido <lgargar2@teleco.upv.es>
2022-11-22 12:21:01 +0100

    fix an error

--- Banco de registros/banco_registros.sv
+++ Banco de registros/banco_registros.sv
@@ -1,4 +1,4 @@
-module banco_registros (CLK,RESET,reg1r,reg2r,regW, writeData,RegWrite,Data1,Data2);
+module banco_registros (CLK, RESET, reg1r, reg2r, regW, writeData, RegWrite, Data1, Data2);
   
 	input CLK,RESET;
     input [SIZE-1:0] reg1r, reg2r, regW; //seleccion de registro
