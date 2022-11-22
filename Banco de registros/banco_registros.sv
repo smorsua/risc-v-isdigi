e28356d8013d1aaa488319cf844a0f4eb17533ff
luisgaarrido <lgargar2@teleco.upv.es>
2022-11-22 17:41:39 +0100

    fixed an input

--- Banco de registros/banco_registros.sv
+++ Banco de registros/banco_registros.sv
@@ -1,6 +1,6 @@
 module banco_registros (CLK, RESET, reg1r, reg2r, regW, writeData, RegWrite, Data1, Data2);
   
-	input CLK,RESET;
+	input CLK, RESET;
     input [SIZE-1:0] reg1r, reg2r, regW; //seleccion de registro
     input  [SIZE-1:0] writeData;
     input RegWrite;
