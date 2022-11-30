3778c5c15cb61038b8839598c86381deef607717
Sergio Moreno Suay <morenosuaysergio@gmail.com>
2022-11-30 09:10:33 +0100

    Updated control

--- single_cycle/CONTROL.sv
+++ single_cycle/CONTROL.sv
@@ -8,7 +8,8 @@ module CONTROL(
     output ALU_SRC,
     output REG_WRITE
 );
-//FIXME: esto no funcionara hasta que el enum de los formatos de instruccion no sea global
+`include "instruction_type.sv"
+
 always_comb begin
     case (INSTRUCTION_FORMAT)
         R_FORMAT: begin
@@ -61,13 +62,13 @@ always_comb begin
         end
         J_FORMAT: 
         begin
-            BRANCH = 0;
-            MEM_READ = 1;
-            MEM_TO_REG = 1;
+            BRANCH = 1; // check
+            MEM_READ = 0; //check 
+            MEM_TO_REG = 0;//check
             ALU_OP = 'b10;
-            MEM_WRITE = 0;
-            ALU_SRC = 1;
-            REG_WRITE = 1;
+            MEM_WRITE = 0;//check
+            ALU_SRC = 1;//check
+            REG_WRITE = 1;//check
         end
         default: 
     endcase
