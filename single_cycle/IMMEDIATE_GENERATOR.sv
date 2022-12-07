88f83deb8253b3c8be587cd8b64036aa7a751208
Sergio Moreno Suay <morenosuaysergio@gmail.com>
2022-12-07 09:58:43 +0100

    FIXED IMM GEN

--- single_cycle/IMMEDIATE_GENERATOR.sv
+++ single_cycle/IMMEDIATE_GENERATOR.sv
@@ -9,7 +9,8 @@ always_comb begin
     I_FORMAT: IMMEDIATE = { {21{inst[31]}}, inst[30:20] };
     S_FORMAT: IMMEDIATE = { {21{inst[31]}}, inst[30:25], inst[11:7] };
     B_FORMAT: IMMEDIATE = { {20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0};
-    U_FORMAT: IMMEDIATE = { inst[31:12], 12'b0 };
+    AUIPC: IMMEDIATE = { inst[31:12], 12'b0 };
+    LUI: IMMEDIATE = { inst[31:12], 12'b0 };
     //J_FORMAT: IMMEDIATE = { {12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
     default: IMMEDIATE = 32'd0;
     endcase
