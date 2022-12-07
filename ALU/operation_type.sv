9a66c72b31c9b1336614af4ddcdc4df3483b16f8
Sergio Moreno Suay <morenosuaysergio@gmail.com>
2022-12-07 08:15:05 +0100

    Reverted operations

--- ALU/operation_type.sv
+++ ALU/operation_type.sv
@@ -1,18 +1,15 @@
 `ifndef OPERATION_TYPE_HEADER
 `define OPERATION_TYPE_HEADER
 typedef enum { 
-	ADD	= 'd0, 
-	SLT	= 'd1, 
-	SLTU	= 'd2,
-	AND	= 'd3, 
-	OR		= 'd4, 
-	XOR	= 'd5,
-	LUI	= 'd6, 
-	AUIPC	= 'd7, 
-	SUB	= 'd8, 
-	BEQ	= 'd9, 
-	BNE	= 'd10,
-	LW		= 'd11, 
-	SW		= 'd12 
-} e_operations;
-`endif //OPERATION_TYPE_HEADER
+	ADD, 
+	SUB, 
+	LESS_THAN, 
+	GREATER_OR_EQUAL_THAN, 
+	AND,
+	OR, 
+	XOR, 
+	LEFT_SHIFT, 
+	SIGNED_LEFT_SHIFT, 
+	RIGHT_SHIFT, 
+	SIGNED_RIGHT_SHIFT } e_operations;
+`endif OPERATION_TYPE_HEADER
