1244ccbab3d92a0c0a2f6f323111e166251a8809
Sergio Moreno Suay <morenosuaysergio@gmail.com>
2022-12-09 13:20:16 +0100

    instruction to test j format

--- instructions.s
+++ instructions.s
@@ -1,18 +1,7 @@
-li x1 40
-li x2 40
-beq x1 x2 Label
-addi x3 x0 1
-addi x3 x0 2
-addi x3 x0 3
-addi x3 x0 4
-addi x3 x0 5
-
-Label:
-addi x3 x0 100
-
-li x1 35
-
-beq x1 x2 Label2
-addi x3 x0 0
-Label2:
-addi x3 x0 101
+j Loop
+li x1 12
+nop
+nop
+nop
+Loop:
+li x1 99
