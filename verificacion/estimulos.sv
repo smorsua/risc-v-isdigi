ba77fe7d904ff58af19678690ff89c647d189d13
Mario <marioricoyecla@hotmail.es>
2022-12-10 16:33:43 +0100

    added: stimulus

--- verificacion/estimulos.sv
+++ verificacion/estimulos.sv
@@ -15,6 +15,9 @@ logic [31:0] instruccion_random;
       $display("probando r_format" );
       generar_instrucciones.R_format.constraint_mode(1);
       prueba_random_r_format();
+      // generar_instrucciones.R_format.constraint_mode(0);
+      // generar_instrucciones.I_format.constraint_mode(1);
+      // prueba_random_i_format();
       $stop;
       $writememh("salida_random.txt", rom_aleatoria_tb.rom_aleatoria_dut.duv.tipos_paquete);      
       $finish;
@@ -38,4 +41,20 @@ task prueba_random_r_format;
   end
 endtask
 
+task prueba_random_i_format;
+  begin
+      assert (generar_instrucciones.randomize()) else    $fatal("randomization failed");   
+      rom_aleatoria_tb.m1.put(generar_instrucciones.valor);
+      testar_ports.address='0; 
+      #0 monitorizar_instrucciones.instrucciones.sample();  
+      while ( monitorizar_instrucciones.instrucciones.iformat.get_coverage()<100)
+	begin
+	   # 100 ;
+	   assert (generar_instrucciones.randomize()) else    $fatal("randomization failed");     
+     	   rom_aleatoria_tb.m1.put(generar_instrucciones.valor);
+	   testar_ports.address= testar_ports.address +1;
+           #0 monitorizar_instrucciones.instrucciones.sample();
+        end
+  end
+endtask
 endprogram
--- verificacion/utilidades_verificacion.sv
+++ verificacion/utilidades_verificacion.sv
@@ -8,13 +8,14 @@ class RCSG_RISCV;
   constraint R_format_a  {(valor[6:0] == 7'b0110011 && valor[14:12]!=3'b000 && valor[14:12]!=3'b101) -> valor[31:25]==7'b0000000 ;}
   constraint R_format_b  {(valor[6:0] == 7'b0110011 && (valor[14:12]==3'b000) || valor[14:12]==3'b101) -> valor[31:25]==7'b0000000 || valor[31:25]==7'b0100000 ;}
   
-  constraint I_format_a    {valor[6:0] == 7'b0010011 -> !(valor[14:12]==3'bx01);} 
-  constraint I_format_b    {valor[6:0] == 7'b0000011 -> valor[14:12]==3'b010}
+  // constraint I_format      {valor[6:0] == 7'b0010011 || valor[6:0] == 7'b0000011;}
+  // constraint I_format_a    {valor[6:0] == 7'b0010011 -> valor[14:12]!=3'bx01;} 
+  // constraint I_format_b    {valor[6:0] == 7'b0000011 -> valor[14:12]==3'b010;}
 
-  constraint S_format    {valor[6:0] == 7'b0100011;}
-  constraint B_format    {valor[6:0] == 7'b1100011;}
-  constraint U_format    {valor[6:0] == 7'b0010111;} 
-  constraint J_format    {valor[6:0] == 7'b1101111;}
+  // constraint S_format    {valor[6:0] == 7'b0100011;}
+  // constraint B_format    {valor[6:0] == 7'b1100011;}
+  // constraint U_format    {valor[6:0] == 7'b0010111;} 
+  // constraint J_format    {valor[6:0] == 7'b1101111;}
   
 endclass
 
@@ -24,30 +25,29 @@ covergroup instrucciones ;
 
   rformat : coverpoint ({monitor_port.dato[30],monitor_port.dato[14:12]})  iff (monitor_port.dato[6:0]==7'b0110011 && monitor_port.dato[31]==1'b0 && monitor_port.dato[29:25]==5'b0000)
   {
-      bins add ={0};
-      bins sub={8};      
-      bins sll={1};
-      bins slt={2};
-      bins sltu={3};
-      bins ixor={4};
-      bins slr={5};
-      bins sra={13};
-      bins ior={6};
-      bins iand={7};
-      illegal_bins imposibles_rformat={9,10,11,12,14,15}; 
+      bins add = {0};
+      bins sub = {8};      
+      bins sll = {1};
+      bins slt = {2};
+      bins sltu = {3};
+      bins ixor = {4};
+      bins slr = {5};
+      bins sra = {13};
+      bins ior = {6};
+      bins iand = {7};
+      illegal_bins imposibles_rformat = {9,10,11,12,14,15}; 
   } 
 
-  iformat : coverpoint ({monitor_port.dato[4],monitor_port.dato[14:12]}) iff (monitor_port.dato[6:0]==7'b00x0011)
-  {
-      bins addi ={8};//1000  
-      bins slti={10};//1010
-      bins xori={12};//1100
-      bins ori={14};//1110
-      bins andi={15};//1111   
-      bins 
-    
-      illegal_bins imposibles_iformat_a={1,3,5}; 
-  } 
+//   iformat : coverpoint ({monitor_port.dato[4],monitor_port.dato[14:12]}) iff (monitor_port.dato[6:0]==7'b00x0011)
+//   {
+//       bins addi = {8};//1000  
+//       bins slti = {10};//1010
+//       bins xori = {12};//1100
+//       bins ori = {14};//1110
+//       bins andi = {15};//1111   
+//       bins lw  = {2}; //0010
+//       illegal_bins imposibles_iformat_a = {0,1,3,4,5,6,7,9,11,13}; 
+//   } 
 
 //   iformat_b : coverpoint ({monitor_port.dato[30],monitor_port.dato[14:12]}) iff (monitor_port.dato[6:0]==7'b0010011&&monitor_port.dato[31]==1'b0)
 //   {
@@ -63,33 +63,33 @@ covergroup instrucciones ;
 //       bins lh={1}; //001
 //       bins lw={2}; //010
 //       illegal_bins imposibles_iformat_load = {3,4,5,6,7}; 
-//   }
+// //   }
 
-sformat : coverpoint ({monitor_port.dato[14:12]})  iff (monitor_port.dato[6:0]==7'b0100011)
-  {
-      bins sb ={0}; //000
-      bins sh={1};  //001 
-      bins sw={2};  //010
-      illegal_bins imposibles_sformat={3,4,5,6,7}; 
-  } 
+// sformat : coverpoint ({monitor_port.dato[14:12]})  iff (monitor_port.dato[6:0]==7'b0100011)
+//   {
+//       bins sb ={0}; //000
+//       bins sh={1};  //001 
+//       bins sw={2};  //010
+//       illegal_bins imposibles_sformat={3,4,5,6,7}; 
+//   } 
 
-bformat : coverpoint ({monitor_port.dato[14:12]})  iff (monitor_port.dato[6:0]==7'b1100011)
-  {
-      bins beq ={0}; //000
-      bins bne ={1};  //001 
-      bins blt ={4};  //010
-      bins bge ={5};  //010
-      bins bltu ={6};  //010
+// bformat : coverpoint ({monitor_port.dato[14:12]})  iff (monitor_port.dato[6:0]==7'b1100011)
+//   {
+//       bins beq ={0}; //000
+//       bins bne ={1};  //001 
+//       bins blt ={4};  //010
+//       bins bge ={5};  //010
+//       bins bltu ={6};  //010
 
-      illegal_bins imposibles_bformat={2,3,7}; 
-  } 
+//       illegal_bins imposibles_bformat={2,3,7}; 
+//   } 
 
-uformat : coverpoint ({monitor_port.dato[6:0]}) iff (monitor_port.dato[6:0]==7'b0x10111)
-  {
-      bins lui ={0110111};
-      bins auipc ={0010111};
-      //illegal_bins imposibles_bformat={}; 
-  } 
+// uformat : coverpoint ({monitor_port.dato[6:0]}) iff (monitor_port.dato[6:0]==7'b0x10111)
+//   {
+//       bins lui ={0110111};
+//       bins auipc ={0010111};
+//       //illegal_bins imposibles_bformat={}; 
+//   } 
 endgroup
 
 function new(virtual if_rom.monitorizar mpuertos);
