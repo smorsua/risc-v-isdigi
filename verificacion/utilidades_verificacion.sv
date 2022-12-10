d09de44b80e69d595c89ead8aff1702297a0b724
luisgaarrido <lgargar2@teleco.upv.es>
2022-12-10 13:49:53 +0100

    constraints

--- verificacion/utilidades_verificacion.sv
+++ verificacion/utilidades_verificacion.sv
@@ -2,18 +2,17 @@ package utilidades_verificacion;
 
 typedef mailbox #(logic [31:0]) instruction_box;
 
-class RCSG_RISCV;
+class RCSG_RISCV; 
   rand logic [31:0] valor;
   constraint R_format    {valor[6:0] == 7'b0110011;}
   constraint R_format_a  {(valor[6:0] == 7'b0110011 && valor[14:12]!=3'b000 && valor[14:12]!=3'b101) -> valor[31:25]==7'b0000000 ;}
   constraint R_format_b  {(valor[6:0] == 7'b0110011 && (valor[14:12]==3'b000) || valor[14:12]==3'b101) -> valor[31:25]==7'b0000000 || valor[31:25]==7'b0100000 ;}
   constraint I_format    {valor[6:0] == 7'b0010011;}
-  constraint I_format_a  {(valor[6:0] == 7'b0010011 && (valor[14:12]== 3'b001)) -> valor[31:25] == 7'b0000000;}
-  constraint I_format_b  {(valor[6:0] == 7'b0010011 && (valor[14:12]== 3'b101)) -> valor[31:25] == 7'b0000000 || valor[31:25] == 7'b0100000;} 
-  //constraint I_format_c  {(valor[6:0] == 7'b0010011 && (valor[14:12]!= 3'b001 && valor[14:12]!=3'b101)) -> valor[31:20] == imm[11:0];}
-  // falta algo en las I-format por el tema del valor que se carga, algo como valor[31:20] == imm[11:0] 
-  //constraint S_format    {(valor[6:0] == 7'b0100011 && (valor[14:12]== ))}
-  //constraint B_format    {(valor[6:0] == 7'b1100011 && (valor[14:12]== ))}
+  
+  constraint S_format    {valor[6:0] == 7'b0100011;}
+  constraint B_format    {valor[6:0] == 7'b1100011;}
+  constraint U_format    {valor[6:0] == 7'b0010111;}
+  constraint J_format    {valor[6:0] == 7'b1101111;}
   
 endclass
 
@@ -35,6 +34,26 @@ covergroup instrucciones ;
       bins iand={7};
       illegal_bins imposibles_rformat={9,10,11,12,14,15}; 
   } 
+
+  iformat_a : coverpoint (monitor_port.dato[14:12]) iff (monitor_port.dato[6:0]==7'b0010011)
+  {
+      bins addi ={0};  
+      bins slti={2};
+      bins xori={4};
+      bins ori={6};
+      bins andi={7};   
+    
+      illegal_bins imposibles_iformat_a={1,3,5}; 
+  } 
+
+  iformat_b : coverpoint ({monitor_port.dato[30],monitor_port.dato[14:12]}) iff (monitor_port.dato[6:0]==7'b0010011&&monitor_port.dato[31]==1'b0)
+  {
+      bins slli={1};
+      bins slri={5};
+      bins srai={13};
+      illegal_bins imposibles_iformat_b={0,2,3,4,6,7,8,9,10,11,12,14,15}; 
+  }
+
 endgroup
 function new(virtual if_rom.monitorizar mpuertos);
 	monitor_port=mpuertos;
