d326f2b55884b92dcbb4bef97fdbc241843b03c2
Sergio <smorsua@teleco.upv.es>
2022-12-10 12:44:50 +0100

    Fixed bubble sort

--- bubble_sort.s
+++ bubble_sort.s
@@ -3,22 +3,28 @@ mv x6 sp # Final array element
 
 #Add values to array
 li x10 1
-jal x1 Push
+sw x10 0(sp)
+addi sp sp -4
 
 li x10 2
-jal x1 Push
+sw x10 0(sp)
+addi sp sp -4
 
 li x10 3
-jal x1 Push
+sw x10 0(sp)
+addi sp sp -4
 
 li x10 4
-jal x1 Push
+sw x10 0(sp)
+addi sp sp -4
 
 li x10 5
-jal x1 Push
+sw x10 0(sp)
+addi sp sp -4
 
 li x10 6
-jal x1 Push
+sw x10 0(sp)
+addi sp sp -4
 
 li x7 0 # didntSwap (bool)
 li x8 1 # Constant = 1
@@ -30,37 +36,27 @@ WhileLoop:
     beq x7 x8 End
     mv x28 sp # i = sp
     addi x28 x28 4 # Primer elemento del array
-     li x7 1
+    li x7 1
     ForLoop:
-    bge x28 x6 EndFor
+        bge x28 x6 EndFor
 
-    lw x10 0(x28)
-    addi x28 x28 4
-    lw x11 0(x28)
-    addi x28 x28 -4
+        lw x10 0(x28)
+        addi x28 x28 4
+        lw x11 0(x28)
+        addi x28 x28 -4
 
-    ble x10 x11 NotSwap
-    li x7 0
-    jal x1 Swap
-    sw x10 0(x28)
-    addi x28 x28 4
-    sw x11 0(x28)
-    addi x28 x28 -4
-NotSwap:
-    addi x28 x28 4
-    j ForLoop
+        ble x10 x11 NotSwap
+        li x7 0
+        mv x12 x10
+   		mv x10 x11
+    	mv x11 x12
+        sw x10 0(x28)
+        addi x28 x28 4
+        sw x11 0(x28)
+        addi x28 x28 -4
+        NotSwap:
+            addi x28 x28 4
+            beq x0 x0 ForLoop
     EndFor:
-    j WhileLoop
-
-Swap: # Swap x10 and x11
-    mv x12 x10
-    mv x10 x11
-    mv x11 x12
-    jalr x0 x1 0
-
-Push: #push x10 to stack
-    sw x10 0(sp)
-    addi sp sp -4
-    jalr x0 x1 0
-
+    	beq x0 x0 WhileLoop
 End:
