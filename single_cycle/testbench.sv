72f29d5d0d8a579b41add1f51a4db4732f5b01dc
laurarimi <laurarimi@hotmail.com>
2022-12-12 10:19:45 +0100

    changed signals name

--- single_cycle/testbench.sv
+++ single_cycle/testbench.sv
@@ -5,23 +5,23 @@ localparam  T = 20, addr_width = 10, data_width = 32;
 
 logic CLK;
 logic RESET_N;
-logic ENABLE_W;
-logic  [(data_width-1):0] Q_W;
-logic [(addr_width-1):0] ADDR_RAM;
-logic  [(data_width-1):0] Q_RAM;
-logic [(addr_width-1):0] ADDR_ROM;
-logic  [(data_width-1):0] Q_ROM;
+logic d_rw;
+logic  [(data_width-1):0] ddata_w;
+logic [(addr_width-1):0] daddr;
+logic  [(data_width-1):0] ddata_r;
+logic [(addr_width-1):0] iaddr;
+logic  [(data_width-1):0] idata;
 
-RAM ram(CLK, ADDR_RAM, ENABLE_W, Q_W, ADDR_RAM, Q_RAM);
+RAM ram(CLK, daddr, d_rw,ddata_w, daddr, ddata_r);
 defparam ram.addr_width = addr_width;
 defparam ram.data_width = data_width;
 
-ROM rom(.ADDR_R(ADDR_ROM), .Q_R(Q_ROM));
+ROM rom(.ADDR_R(iaddr), .Q_R(idata));
 defparam rom.addr_width = addr_width;
 defparam rom.data_width = data_width;
 defparam rom.file = "instructions.txt" ;
 
-main main(CLK, RESET_N, Q_ROM, ADDR_ROM, ADDR_RAM, Q_RAM, Q_W, ENABLE_W);
+main main(CLK, RESET_N, idata, iaddr, daddr, ddata_r, ddata_w, d_rw);
 defparam main.ADDR_WIDTH = addr_width;
 defparam main.SIZE = data_width;
 
