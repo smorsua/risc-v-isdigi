95863d443435bc77918931a09bdeb1a8eb866b28
Your Name <you@example.com>
2022-12-14 09:53:41 +0100

    banco_reg sincrono

--- Banco de registros/banco_registros.sv
+++ Banco de registros/banco_registros.sv
@@ -1,43 +1,36 @@
-module BANCO_REGISTROS #(parameter SIZE = 32) (
-    CLK,
-    RESET_N,
-    read_reg1,
-    read_reg2,
-    write_reg,
-    writeData,
-    RegWrite,
-    Data1,
-    Data2);
-    
-    input CLK, RESET_N;
-    input logic [$clog2(SIZE)-1:0] read_reg1, read_reg2, write_reg;
+module banco_registros #(parameter SIZE = 32) (CLK,RESET,reg1r,reg2r,regW, writeData,RegWrite,Data1,Data2);
+  
+    input CLK,RESET;
+    input logic [$clog2(SIZE)-1:0] reg1r, reg2r, regW; //seleccion de registro DIRECCIONES
     input logic [SIZE-1:0] writeData;
     input RegWrite;
-    output logic [SIZE-1:0] Data1, Data2; 
+    output logic [SIZE-1:0] Data1, Data2; //dentro de cada registro, X0,X1 etc selecciono los datos de 32bits de cada registro
  logic [SIZE-1:0][31:0] banco_registros;
 
-always @(posedge CLK or negedge RESET_N)
-begin 
-    if(!RESET_N)
-	    banco_registros <= '0; 
-    else if(RegWrite && write_reg != '0)
-        banco_registros[write_reg] <= writeData;
-end  
+//caso escritura
+//cojo el valor de regW que es la direccion donde voy a escribir el dato que obtengo de la señal writeData 
 
-assign Data1 = banco_registros[read_reg1];
-assign Data2 = banco_registros[read_reg2];
+always @(posedge CLK or negedge RESET)
+begin 
+    if(!RESET)
+	 banco_registros <= '0;  
+
+    else if(RegWrite && regW!='0)
+        banco_registros[regW] <= writeData;
+
+end     
 
 //caso lectura
 
-assign Data1 = banco_registros[reg1r];
-assign Data2 = banco_registros[reg2r];
+always_ff @(posedge CLK)
+	begin
+		Data1 <= banco_registros[reg1r];  
+		Data2 <= banco_registros[reg2r];
+	end
 
-// problema a considerar: lectura y escritura del mismo registro 
-//asertions
-assert property (@(posedge CLK) disable iff (RegWrite and regW!='0) |-> (writeData == banco_registros[regW])) else $error("No realiza correctamente la escritura")
-//no sé si debería utilizar ##1 para así seleccionar diferentes ciclos, osea muestrear writeData 
-//del ciclo anterior con lo que me saca banco_registros[regW] en el ciclo actual
-endmodule 
+
+//assert property (@(posedge CLK) disable iff (RegWrite and regW!='0) |-> (writeData == banco_registros[regW])) else $error("No realiza correctamente la escritura")
+endmodule
 
 
 
