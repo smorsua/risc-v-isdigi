e8501fefee0d76c52f8098c6801374c9f016f8ff
Sergio Moreno Suay <morenosuaysergio@gmail.com>
2022-12-20 18:43:28 +0100

    Fixed fibonacci pipelined (not sure)

--- fibonacci_pipelined.s
+++ fibonacci_pipelined.s
@@ -5,12 +5,16 @@ li x13, 1 # Counter. i = 1 porque voy a guardar el primer termino antes del bucl
 li sp 1000
 nop
 nop
+nop
+nop
+nop
 sw x12 0(sp) # Guardo el primer término
 addi sp, sp, -4
 Loop:
 	bge x13, x10, Done
     mv x14, x12 # Guardo x12 en x14
     add x12, x11, x12 # Guardo en x12 el nuevo término
+    nop
     nop
     nop
     nop
@@ -22,5 +26,12 @@ Loop:
     nop
     nop
     nop
+    nop
+    nop
     beq x0 x0 Loop
+    nop
+    nop
+    nop
+    nop
+    nop
 Done:
