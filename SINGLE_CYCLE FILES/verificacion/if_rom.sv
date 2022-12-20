f830f1141d4518b89b25bd93e924a92646d0aa96
Mario <mriciba@teleco.upv.es>
2022-12-20 17:07:39 +0100

    added: single_cycle files

---
+++ SINGLE_CYCLE FILES/verificacion/if_rom.sv
@@ -0,0 +1,25 @@
+`ifndef IF_ROM_GUARD
+`define IF_ROM_GUARD
+
+interface if_rom;
+
+logic [9:0] iaddr;
+logic [31:0] idata;
+
+modport monitorizar
+(input iaddr,
+input idata);
+
+modport duv
+(input iaddr,
+output idata);
+
+modport input_iaddr(
+    input iaddr
+);
+
+modport main_circuit
+(output iaddr,
+input idata);
+endinterface
+`endif //IF_ROM_GUARD
---
+++ SINGLE_CYCLE FILES/verificacion/rom_aleatoria_tb.sv
@@ -0,0 +1,57 @@
+`include "if_ram.sv"
+`include "if_rom.sv"
+`include "../single_cycle/main.sv"
+module ram_with_if(if_ram.ram_module bus);
+  RAM ram(CLK, bus.daddr, bus.d_rw, bus.ddata_w, bus.ddata_r);
+  defparam ram.addr_width = 10;
+  defparam ram.data_width = 32;
+endmodule
+
+module rom_aleatoria_tb ();
+  // Parameters
+  localparam  d_width = 32;
+  localparam  mem_depth = 1024;
+  localparam a_width=$clog2(mem_depth);
+	localparam T = 10;
+
+  
+  logic CLK;
+  logic RESET_N;
+
+  initial
+	begin
+		CLK = 0;
+		forever  #(T/2) CLK=!CLK;
+	end 
+
+	initial begin
+		RESET_N = 0;
+		#(T)
+		RESET_N = 1;
+	end
+
+  utilidades_verificacion::instruction_box m1=new();
+
+  if_rom interface_rom();
+  if_ram interface_ram();
+
+  top_duv rom_aleatoria_dut(.bus(interface_rom.duv));
+
+  estimulos rom_aleatoria_estimulos(.monitorizar_ports(interface_rom.monitorizar));
+
+
+
+
+  main main_circuit(
+      .CLK(CLK),
+      .RESET_N(RESET_N),
+      .idata(interface_rom.main_circuit.idata),
+      .iaddr(interface_rom.main_circuit.iaddr),
+      .daddr(interface_ram.main_circuit.daddr),
+      .ddata_r(interface_ram.main_circuit.ddata_r),
+      .ddata_w(interface_ram.main_circuit.ddata_w),
+      .d_rw(interface_ram.main_circuit.d_rw)
+  );
+
+ram_with_if ram_con_interfaz(interface_ram.ram_module);
+endmodule
