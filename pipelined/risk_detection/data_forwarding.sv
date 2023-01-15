024a1f29a77ae92d91d3d1d68a485bc862a6e1ea
Sergio <smorsua@teleco.upv.es>
2023-01-15 22:25:05 +0100

    Change data forwarding names

--- pipelined/risk_detection/data_forwarding.sv
+++ pipelined/risk_detection/data_forwarding.sv
@@ -1,8 +1,8 @@
 module data_forwarding #(parameter SIZE=32) (
     input reg_write_mem, reg_write_ex, reg_write_wb, reg_write_wb_aux,
-    input [4:0] inst_11_to_7_mem, inst_11_to_7_wb, inst_11_to_7_wb_aux,
-    input [4:0] inst_19_to_15_ex, inst_24_to_20_ex,
-    input [6:0] inst_6_to_0_ex,
+    input [4:0] rd_mem, rd_wb, rd_wb_aux,
+    input [4:0] rs1_ex, rs2_ex,
+    input [6:0] opcode_ex,
     output logic [1:0] forwardA, forwardB
 );
 
@@ -10,33 +10,33 @@ module data_forwarding #(parameter SIZE=32) (
 //DEBIDO A QUE SON LÁS ÚNICAS QUE TIENEN RS1 Y RS2. 
 always_comb begin 
 //RSB format
-if(inst_6_to_0_ex == 7'b0110011 || inst_6_to_0_ex == 7'b0100011 || inst_6_to_0_ex == 7'b1100011) begin
-    if(reg_write_mem && (inst_11_to_7_mem != 0) && (inst_11_to_7_mem == inst_19_to_15_ex))
+if(opcode_ex == 7'b0110011 || opcode_ex == 7'b0100011 || opcode_ex == 7'b1100011) begin
+    if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs1_ex))
         forwardA = 10;
-    else if(reg_write_wb && (inst_11_to_7_wb != 0) && (inst_11_to_7_wb == inst_19_to_15_ex))
+    else if(reg_write_wb && (rd_wb != 0) && (rd_wb == rs1_ex))
         forwardA = 01;
-    else if(reg_write_wb_aux && (inst_11_to_7_wb_aux != 0) && (inst_11_to_7_wb_aux == inst_19_to_15_ex))
+    else if(reg_write_wb_aux && (rd_wb_aux != 0) && (rd_wb_aux == rs1_ex))
         forwardA = 11;
     else
         forwardA = 00;
 
 
-    if(reg_write_mem && (inst_11_to_7_mem != 0) && (inst_11_to_7_mem == inst_24_to_20_ex))
+    if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs2_ex))
         forwardB = 10;
-    else if(reg_write_wb && (inst_11_to_7_wb != 0) && (inst_11_to_7_wb == inst_24_to_20_ex))
+    else if(reg_write_wb && (rd_wb != 0) && (rd_wb == rs2_ex))
         forwardB = 01;
-    else if(reg_write_wb_aux && (inst_11_to_7_wb_aux != 0) && (inst_11_to_7_wb_aux == inst_24_to_20_ex))
+    else if(reg_write_wb_aux && (rd_wb_aux != 0) && (rd_wb_aux == rs2_ex))
         forwardB = 11;
     else
         forwardB = 00;
 end
-else if (inst_6_to_0_ex == 7'b0010011) begin
+else if (opcode_ex == 7'b0010011) begin
     
-    if(reg_write_mem && (inst_11_to_7_mem != 0) && (inst_11_to_7_mem == inst_19_to_15_ex))
+    if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs1_ex))
         forwardA = 10;
-    else if(reg_write_wb && (inst_11_to_7_wb != 0) && (inst_11_to_7_wb == inst_19_to_15_ex))
+    else if(reg_write_wb && (rd_wb != 0) && (rd_wb == rs1_ex))
         forwardA = 01;
-    else if(reg_write_wb_aux && (inst_11_to_7_wb_aux != 0) && (inst_11_to_7_wb_aux == inst_19_to_15_ex))
+    else if(reg_write_wb_aux && (rd_wb_aux != 0) && (rd_wb_aux == rs1_ex))
         forwardA = 11;
     else
         forwardA = 00;
