070d51ae68c6a41d39e36b07ced696859adab5bb
laurarimi <laurarimi@hotmail.com>
2023-01-16 13:16:21 +0100

    last version

--- FASE4/single_cycle/cochefantastico.sv
+++ FASE4/single_cycle/cochefantastico.sv
@@ -1,5 +1,7 @@
-`ifndef PIPELINED_GUARD
-`define PIPELINED_GUARD
+
+`include "../Shared/MUX.sv"
+`ifndef SINGLE_CYCLE_GUARD
+`define SINGLE_CYCLE_GUARD
 
 `include "../Shared/MUX.sv"
 `include "../Shared/ALU/ALU.sv"
@@ -9,22 +11,15 @@
 `include "../Shared/Control/IMMEDIATE_GENERATOR.sv"
 `include "../Shared/Control/instruction_type.sv"
 
-`include "./banco_registros/banco_registros_registered.sv"
-`include "./memories/ram_registered.sv"
-`include "./memories/rom_registered.sv"
-
-`include "./pipelined_registers/IF_ID_REG.sv"
-`include "./pipelined_registers/ID_EX_REG.sv"
-`include "./pipelined_registers/EX_MEM_REG.sv"
-`include "./pipelined_registers/MEM_WB_REG.sv"
-
-`include "./risk_detection/hazard_detection.sv"
+`include "./banco_registros/banco_registros_unregistered.sv"
+`include "./memories/ram_unregistered.sv"
+`include "./memories/rom_unregistered.sv"
 
 module cochefantastico
 #(parameter DATA_SIZE = 32, parameter ADDR_SIZE = 10)(
     input                   CLK,
-    input                   RESET_N,
-    input                   CLEAR
+    input                   RESET_N
+
 
 );
 
@@ -41,42 +36,46 @@ wire reg_write_enable;
 wire [DATA_SIZE-1:0] reg_write_data;
 wire [4:0] write_register;
 
-pipelined pipelined(CLK, RESET_N,CLEAR, idata, iaddr, daddr, ddata_r, ddata_w, mem_write, mem_read,reg_write_data, reg_write_enable, write_register);
-defparam pipelined.ADDR_SIZE = ADDR_SIZE;
-defparam pipelined.DATA_SIZE = DATA_SIZE;
+single_cycle main(CLK, RESET_N, idata, iaddr, daddr, ddata_r, ddata_w, d_rw);
+defparam main.ADDR_WIDTH = ADDR_SIZE;
+defparam main.SIZE = DATA_SIZE;
 
 wire [DATA_SIZE-1:0]ddata_r_ram;
-wire [DATA_SIZE-1:0]ddata_w_ram;
+//wire [DATA_SIZE-1:0]ddata_w;
 wire [ADDR_SIZE-1:0] daddr_ram;
-wire mem_read_ram;
-wire mem_write_ram;
-ram_registered ram_registered(CLK, daddr_ram, mem_write_ram, mem_read_ram, ddata_w_ram, ddata_r_ram);
-defparam ram_registered.addr_width = ADDR_SIZE;
-defparam ram_registered.data_width = DATA_SIZE;
+wire d_rw_ram;
 
-rom_registered rom_registered(.CLK(CLK), .iaddr(iaddr), .idata(idata));
-defparam rom_registered.addr_width = ADDR_SIZE;
-defparam rom_registered.data_width = DATA_SIZE;
-defparam rom_registered.file = "./leds_placa.txt" ;
 
-wire enable_GPIO; //ENABLE DE ESCRITURA
-wire mem_read_GPIO; //ENABLE DE LECTURA
-wire [DATA_SIZE-1:0] ddata_w_GPIO, ddata_r_GPIO;
+
+ram_unregistered ram_unregistered(CLK, daddr_ram, d_rw_ram, ddata_w, ddata_r_ram);
+defparam ram_unregistered.addr_width = ADDR_SIZE;
+defparam ram_unregistered.data_width = DATA_SIZE;
+
+rom_unregistered rom_unregistered(.iaddr(iaddr), .idata(idata));
+defparam rom_unregistered.addr_width = ADDR_SIZE;
+defparam rom_unregistered.data_width = DATA_SIZE;
+defparam rom_unregistered.file = "./leds_placa.txt" ;
+
+
+wire d_rw_GPIO;
+wire [DATA_SIZE-1:0]  ddata_r_GPIO;
 wire [ADDR_SIZE-1:0] daddr_GPIO;
-ram_registered ram_GPIO(CLK, daddr_GPIO, enable_GPIO, mem_read_GPIO, ddata_w_GPIO, ddata_r_GPIO);
+
+ram_unregistered ram_GPIO(CLK, daddr_GPIO, d_rw_GPIO, ddata_w, ddata_r_GPIO);
 defparam ram_GPIO.addr_width = ADDR_SIZE;
 defparam ram_GPIO.data_width = DATA_SIZE;
+
 
 reg addr12_reg;
 always @(posedge CLK)
     begin
-        addr12_reg<= daddr[12];
+        addr12_reg<= daddr[9];
 
     end
 
 ////////////////////////////////////INVERSOR(HACIA PLACA)\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-assign {mem_write_ram,mem_read_ram} = (daddr[12] == 1'b0)? {mem_write,mem_read}:1'b0;
-assign {mem_write_GPIO,mem_read_GPIO} = (daddr[12] == 1'b1)? {mem_write,mem_read}:1'b0;
+assign d_rw_ram = (daddr[9] == 1'b0)? d_rw:1'b0;
+assign d_rw_GPIO = !d_rw_ram;
 //////////////////////////////////////////////MUX(HACIA MICRO)\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 wire [DATA_SIZE-1:0] myInput_mem_controller[2];
 assign myInput_mem_controller[0] = ddata_r_ram;
--- FASE4/single_cycle/testbenches/testbench_cochefantastico.sv
+++ FASE4/single_cycle/testbenches/testbench_cochefantastico.sv
@@ -1,8 +1,5 @@
  `timescale 1ns/1ps
 
-`include "../memories/ram_registered.sv"
-`include "../memories/rom_registered.sv"
-`include "../pipelined.sv"
 
 module testbench_cochefantastico();
 localparam  T = 20, addr_width = 10, data_width = 32;
@@ -10,12 +7,12 @@ localparam  T = 20, addr_width = 10, data_width = 32;
 
 logic CLK;
 logic RESET_N;
-logic CLEAR;
+
 
 cochefantastico cochefantastico(
     .CLK(CLK),
-    .RESET_N(RESET_N),
-    .CLEAR(CLEAR)
+    .RESET_N(RESET_N)
+
 
     );
 defparam cochefantastico.ADDR_SIZE = addr_width;
@@ -30,10 +27,8 @@ end
 initial
     begin
         RESET_N = 0;
-        CLEAR = 1;
 		    #(T)
 		RESET_N = 1;
-        CLEAR = 0;
         #(T*100000);
         $stop;
 
