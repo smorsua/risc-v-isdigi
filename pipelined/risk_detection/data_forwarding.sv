f6e2fde8fd1bb5f248f7680c6ca11518c8f179ab
Sergio <smorsua@teleco.upv.es>
2023-01-16 13:06:51 +0100

    Changed names in data forwarding

--- pipelined/risk_detection/data_forwarding.sv
+++ pipelined/risk_detection/data_forwarding.sv
@@ -1,5 +1,8 @@
 `ifndef DATA_FORWARDING_GUARD
 `define DATA_FORWARDING_GUARD
+
+`include "../../Shared/Control/instruction_type.sv"
+
 module data_forwarding #(parameter SIZE) (
     input reg_write_mem, reg_write_ex, reg_write_wb, reg_write_wb_aux,
     input [4:0] rd_mem, rd_wb, rd_wb_aux,
@@ -16,7 +19,22 @@ module data_forwarding #(parameter SIZE) (
 
 always_comb begin 
 //RSB format
-if(opcode_ex == 7'b0110011 || opcode_ex == 7'b1100011) begin
+if(opcode_ex == R_FORMAT || opcode_ex == B_FORMAT) begin
+    r_format_and_b_format_forwarding();
+end
+else if(opcode_ex == S_FORMAT) begin
+    s_format_forwarding();
+end
+else if (opcode_ex == I_FORMAT) begin    
+    i_format_forwarding();
+end
+else begin // U_FORMAT and J_FORMAT
+    forwardA = 2'b00;
+    forwardB = 2'b00;
+end
+end
+
+task r_format_and_b_format_forwarding();
     if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs1_ex))
         forwardA = 2'b10;
     else if(reg_write_wb && (rd_wb != 0) && (rd_wb == rs1_ex))
@@ -35,9 +53,10 @@ if(opcode_ex == 7'b0110011 || opcode_ex == 7'b1100011) begin
         forwardB = 2'b11;
     else
         forwardB = 2'b00;
-end
-else if(opcode_ex == 7'b0100011)begin
-     if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs1_ex))
+endtask
+
+task s_format_forwarding();
+    if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs1_ex))
         forwardA = 2'b10;
     else if(reg_write_wb && (rd_wb != 0) && (rd_wb == rs1_ex))
         forwardA = 2'b01;
@@ -47,9 +66,9 @@ else if(opcode_ex == 7'b0100011)begin
         forwardA = 2'b00;
 
     forwardB = 2'b00;
-end
-else if (opcode_ex == 7'b0010011) begin
-    
+endtask
+
+task i_format_forwarding();
     if(reg_write_mem && (rd_mem != 0) && (rd_mem == rs1_ex))
         forwardA = 2'b10;
     else if(reg_write_wb && (rd_wb != 0) && (rd_wb == rs1_ex))
@@ -60,12 +79,7 @@ else if (opcode_ex == 7'b0010011) begin
         forwardA = 2'b00;
     
     forwardB = 2'b00;
-end
-else begin
-    forwardA = 2'b00;
-    forwardB = 2'b00;
-end
-end
+endtask
 
 endmodule
 `endif
